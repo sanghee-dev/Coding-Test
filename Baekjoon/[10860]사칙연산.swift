dcbf6a583d36d6be5ea97e24fe6732783cf08dd9
sanghee-dev <leeesangheee@gmail.com>
2021-09-06 20:22:52 +0900

    Create [10860]사칙연산.swift

---
+++ Baekjoon/[10860]사칙연산.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let input = readLine()
+let inputArr = input!.components(separatedBy: " ")
+
+print(Int(inputArr[0])! + Int(inputArr[1])!)
+print(Int(inputArr[0])! - Int(inputArr[1])!)
+print(Int(inputArr[0])! * Int(inputArr[1])!)
+print(Int(inputArr[0])! / Int(inputArr[1])!)
+print(Int(inputArr[0])! % Int(inputArr[1])!)
